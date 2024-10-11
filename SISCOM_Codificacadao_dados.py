c2c1c36c78ab8a1941d663298521904ddd70c10a
Lson11 <elison.maiko@gmail.com>
2024-10-11 17:57:43 -0300

    Initial mapping 8B6T

--- SISCOM_Codificacadao_dados.py
+++ SISCOM_Codificacadao_dados.py
@@ -11,24 +11,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #leitura
-leitura = "d"
+leitura = "dddd"
 while (len(leitura) != 4):
     leitura = input("Insira 4 caracteres:")
 
 #ASCII
-dados = []
-for i in leitura:
-    bin = format(ord(i), '08b')  
-    print(f"Char: {i} --> {ord(i)} --> {bin}")  
-    dados.append(bin)
-dados = ''.join(dados) #usar como int()?
+def conv_ASCII(sinal):
+    dados = []
+    for i in sinal:
+        bin = format(ord(i), '08b')  
+        print(f"Char: {i} --> {ord(i)} --> {bin}")  
+        dados.append(bin)
+    dados = ''.join(dados) #usar como int()?
+    return dados
 
-print (dados, end = "\n\n")
+#print (dados, end = "\n\n")
 
 # Tratamento de dados:
 # Tratar como string, pra facilitar os indices do 
 #---------------------------------------------------------------------
-ex_prof = '0011011001'
+ex_prof = '11001100'
 def cod_2BQ1 (sinal):
     mapping =  {
         '00': 1,
@@ -48,6 +50,29 @@ def cod_2BQ1 (sinal):
             nivel_anterior = -dupla
     return cod
 
-print (cod_2BQ1(ex_prof))
+#print (cod_2BQ1(ex_prof))
+#------------------------------------------------------------------------
+def cod_8B6T(sinal):
+    #Receber string com os bits
+    #Separar de 8 em 8 bits
+    #Converter de string pra binário para:
+    #Converter de Binário pra HEXA (6bits)
+    #Converter de HEXA p string para tratar os dados
+    #Relacionar mapping com hexa
+
+    #Mapeando segundo a Tabela FORUZAN APENDICE D
+    mapping = {
+        '00': [-1,1,0,0,-1,1], '01': [0,-1,1,-1,1,0], '02': [0,-1,1,0,-1,1], '03': [0,-1,1,1,0 ,-1], '04': [-1,1,0,1,0,-1], '05': [1,0,-1,-1,1,0]
+    }
+
+    for i in range (0, len(sinal), 8):
+        strinig_bin = sinal[i:i+8]              #Separa de 8 em 8            
+        bin = int(strinig_bin,2)                #Converete p BIN
+        hexa = format(bin, 'X')                 #Converte p HEXA
+              
+
+    return bin, hexa
+
+print(cod_8B6T(ex_prof))
 
 
