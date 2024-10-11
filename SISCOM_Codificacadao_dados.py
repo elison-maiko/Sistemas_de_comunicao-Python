30e7548835610646afe79f7379dadbec8f2b6ddf
Lson11 <elison.maiko@gmail.com>
2024-10-11 16:28:03 -0300

    Codificação 2BQ1 feita

--- SISCOM_Codificacadao_dados.py
+++ SISCOM_Codificacadao_dados.py
@@ -22,11 +22,32 @@ for i in leitura:
     print(f"Char: {i} --> {ord(i)} --> {bin}")  
     dados.append(bin)
 dados = ''.join(dados) #usar como int()?
-print(dados)
 
-#tratamento de dados:
+print (dados, end = "\n\n")
 
+# Tratamento de dados:
+# Tratar como string, pra facilitar os indices do 
+#---------------------------------------------------------------------
+ex_prof = '0011011001'
+def cod_2BQ1 (sinal):
+    mapping =  {
+        '00': 1,
+        '01': 3,
+        '10': -1,
+        '11': -3
+    }
+    cod = []
+    nivel_anterior = 1
+    for i in range(0, len(sinal), 2):       #Ir pegando duplas de bits
+        dupla = mapping[sinal[i:i+2]]       #converte pra inteiro, de 2 em 2 da string sinal
+        if nivel_anterior > 0:              #condição pra invesões
+            cod.append(dupla)               #adiciona o inteiro a string de codificação
+            nivel_anterior = dupla          #Atualiza o nivel para verificação no proximo loop
+        else :
+            cod.append(-dupla)
+            nivel_anterior = -dupla
+    return cod
 
-
+print (cod_2BQ1(ex_prof))
 
 
